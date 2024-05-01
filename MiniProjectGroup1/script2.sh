d7b09015f60c6a62cd417f828a7a00e2238e2b5a
diyadoshi661 <152588410+diyadoshi661@users.noreply.github.com>
2024-05-01 12:13:08 -0700

    Update script2.sh

--- MiniProjectGroup1/script2.sh
+++ MiniProjectGroup1/script2.sh
@@ -1,28 +1,21 @@
 awk -F, 'NR > 1 {
-    # Remove potential quotes and trim spaces
         gsub(/"/, "", $2);
 	    gsub(/"/, "", $8);
 	        gsub(/"/, "", $11);
 		    
-		    age = $2 + 0;  # Convert to numeric to ensure correct comparison
-		        purchase_amount = $9 + 0;  # Assuming $9 is purchase amount
-			    promotion_used = $11 + 0;  # Assuming $11 is promotion usage (0 or 1)
+		    age = $2 + 0; 
+		        purchase_amount = $9 + 0;  
+			    promotion_used = $11 + 0; 
 
-			        # Classifying age into groups
 				    if (age <= 21) age_group = "Below 21";
 					        else if (age >= 22 && age <= 35) age_group = "22 to 35";
 							    else age_group = "Above 35";
-
-								        # Forming the key with age group and promotion usage
 									    key = age_group SUBSEP promotion_used;
-
-									        # Summing up purchase amounts and counting entries
 										    sum_amount[key] += purchase_amount;
 										        count[key]++;
 
 										} 
 										END {
-										    # Calculating and printing the average purchase amounts
 										        for (key in sum_amount) {
 												        split(key, indices, SUBSEP);
 													age_group = indices[1];
