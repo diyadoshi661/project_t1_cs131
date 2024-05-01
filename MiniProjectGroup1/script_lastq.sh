652c918df644121d9097a44fca88baae42b1dac0
diyadoshi661 <152588410+diyadoshi661@users.noreply.github.com>
2024-05-01 12:28:36 -0700

    Update script_lastq.sh

--- MiniProjectGroup1/script_lastq.sh
+++ MiniProjectGroup1/script_lastq.sh
@@ -1,13 +1,9 @@
 awk -F, 'NR > 1 {
-    region = $6;  # Assuming $6 is the region column
-        loyalty = $7;  # Assuming $7 is the loyalty_status column
-	    purchase = $9;  # Assuming $9 is the purchase_amount column
-
-	        # Summing purchase amounts for each region
+    region = $6;  
+        loyalty = $7;  
+	    purchase = $9; 
 		    region_purchase_sum[region] += purchase;
 		        region_purchase_count[region]++;
-
-			    # Counting loyalty status occurrences for each region
 			        loyalty_status_count[region, loyalty]++;
 			}
 			END {
