9c9862410b13edfed61db4034e0a91e6e928b078
diyadoshi661 <152588410+diyadoshi661@users.noreply.github.com>
2024-05-01 12:26:54 -0700

    Update script4.sh

--- MiniProjectGroup1/script4.sh
+++ MiniProjectGroup1/script4.sh
@@ -1,10 +1,7 @@
 awk -F, 'NR > 1 {
-    # Assuming $8 is the purchase frequency and $9 is the purchase amount
-        gsub(/"/, "", $8);  # Remove quotes if present
-	    gsub(/"/, "", $9);  # Remove quotes if present and convert to numeric
-	        purchase_amount = $9 + 0;  # Convert to numeric to ensure proper addition
-		    
-		    # Accumulate purchase amounts based on purchase frequency
+        gsub(/"/, "", $8);  
+	    gsub(/"/, "", $9); 
+	        purchase_amount = $9 + 0;  
 		        if ($8 == "rare") {
 				        total_rare += purchase_amount;
 					    } else if ($8 == "occasional") {
