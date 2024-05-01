59c39dcdcf5dee8cf2f95ae4c4888baca4dc9c15
diyadoshi661 <152588410+diyadoshi661@users.noreply.github.com>
2024-05-01 12:27:50 -0700

    Update script_food_health.sh

--- MiniProjectGroup1/script_food_health.sh
+++ MiniProjectGroup1/script_food_health.sh
@@ -2,19 +2,13 @@ awk -F, 'BEGIN {
     print "Age Group,Product Category,Purchase Amount";
     }
     NR > 1 {
-        age = $2 + 0;  # Ensure age is treated as a number
-	    category = $10;  # Assuming $10 is the product category column
-	        amount = $9 + 0;  # Ensure amount is treated as a number
-
-		    # Remove quotes from category if present
+        age = $2 + 0;  
+	    category = $10;  
+	        amount = $9 + 0;  
 		        gsub(/"/, "", category);
-
-			    # Defining age categories
 			        if (age < 22) age_group = "Below 22";
 					    else if (age <= 35) age_group = "22 to 35";
 						        else age_group = "Above 35";
-
-								    # Focusing only on Food and Health categories
 								        if (category == "Food" || category == "Health") {
 										        key = age_group SUBSEP category;
 											        purchase_sum[key] += amount;
